40908ae133028420a0af144512da4bdcc50bfba4
madoyakimberley <kimmadoya@gmail.com>
2026-04-01 22:25:00 +0300

    Deployment: Server Fix

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const { createServer } = require("http");
-const { parse } = require("url"); // Add this
-const next = require("next");
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
 
-const dev = false; // Hardcode to false for Namecheap
-const app = next({ dev });
+const dev = false;
+const port = process.env.PORT || 3000;
+
+const app = next({ dev, port });
 const handle = app.getRequestHandler();
-
-const PORT = process.env.PORT || 3000;
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Add the parsedUrl here
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(PORT, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Server running on port ${PORT}`);
+    console.log(`> Ready on port ${port}`);
   });
 });
